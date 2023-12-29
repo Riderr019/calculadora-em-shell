28e6dc64af795cbbd291c83998d94dbc4f5092c3
Riderr019 <154712054+Riderr019@users.noreply.github.com>
2023-12-29 16:42:46 -0300

    bash.sh

--- bash.sh
+++ bash.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
-#-Variaveis
+#-Variaveia
 declare _N1
-declare _N2                                                            
+declare _N2
+
 #-Apresentação
 echo [SEJA BEM-VINDO AO MEU SCRIPT]
 echo "Apenas Escolha 2 numeros e ele ira
-somar
+somar  
 subtrair
-multiplicar e
-dividir
+multiplicar 
+dividir e
+potencializar
 ●❯────────｢⊙｣────────❮●━"
 #-calculos
-echo -n "escolha um numero inteiro: "                                  read _N1                                                               echo  "você escolheu $_N1"                                             echo -n "escolha outro: "                                              read _N2                                                               echo  "você escolheu $_N2"                                                                                                                    
+echo -n "escolha um numero inteiro: "
+read _N1
+echo  "você escolheu $_N1"
+echo -n "escolha outro: "
+read _N2
+echo  "você escolheu $_N2"
+
+
 echo "●❯────────｢⊙｣────────❮●━"
 echo "a soma entre $_N1 e $_N2 é igual a ="  $(($_N1+$_N2))
 echo "a subtração entre $_N1 e $_N2 é igual a ="  $(($_N1-$_N2))
 echo "a multiplicação entre $_N1 e $_N2 é igual a ="  $(($_N1*$_N2))
-echo "a divisão entre $_N1 e $_N2 é igual a ="  $(($_N1/$_N2))
+echo "a divisão entre $_N1 e $_N2 é igual a ="  $(($_N1/$_N2))                
 echo "a potenciação entre $_N1 e $_N2 é igual a =" $(($_N1**$_N2))
-echo "a entre $_N1 e $_N2 é igua a =" $(($_N1+$_N2))
+echo "Feito Por:rider"
