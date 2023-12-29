f20fde96ae111058145d4bc1a7cba17447923a00
Riderr019 <154712054+Riderr019@users.noreply.github.com>
2023-12-29 16:15:10 -0300

    bash.sh

---
+++ bash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#-Variaveis
+declare _N1
+declare _N2                                                            
+#-Apresentação
+echo [SEJA BEM-VINDO AO MEU SCRIPT]
+echo "Apenas Escolha 2 numeros e ele ira
+somar
+subtrair
+multiplicar e
+dividir
+●❯────────｢⊙｣────────❮●━"
+#-calculos
+echo -n "escolha um numero inteiro: "                                  read _N1                                                               echo  "você escolheu $_N1"                                             echo -n "escolha outro: "                                              read _N2                                                               echo  "você escolheu $_N2"                                                                                                                    
+echo "●❯────────｢⊙｣────────❮●━"
+echo "a soma entre $_N1 e $_N2 é igual a ="  $(($_N1+$_N2))
+echo "a subtração entre $_N1 e $_N2 é igual a ="  $(($_N1-$_N2))
+echo "a multiplicação entre $_N1 e $_N2 é igual a ="  $(($_N1*$_N2))
+echo "a divisão entre $_N1 e $_N2 é igual a ="  $(($_N1/$_N2))
+echo "a potenciação entre $_N1 e $_N2 é igual a =" $(($_N1**$_N2))
+echo "a entre $_N1 e $_N2 é igua a =" $(($_N1+$_N2))
